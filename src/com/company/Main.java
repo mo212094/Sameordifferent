7f00b87de44f877293a4bfa4b88bce6194254a3b
Maeve O'Shea <MO212094@reigate.ac.uk>
2021-10-05 15:50:06 +0100

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+impot
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.printLine("What is 2 + 2");
+        String UserOpinion = input.next();
+
+        if (UserOpinion.contains(“4")){
+                System.out.println("You are correct");
+        else {
+            System.out.println("You are wrong");
+        }
+
+        }
+}
